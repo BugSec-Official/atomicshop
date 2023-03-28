95e397a318b0e6f43380228c8048aef6c53c7b8b
Denis <denisk@bugsec.com>
2023-03-28 11:52:50 +0300

    ReadMe Update

---
+++ Utils/GitUpload.cmd
@@ -0,0 +1,14 @@
+echo Check git commit version
+echo Check ReadMe version
+pause
+cd..
+git status
+pause
+echo Check Connection
+pause
+git add .
+pause
+git commit -m "ReadMe Update"
+pause
+git push
+pause
