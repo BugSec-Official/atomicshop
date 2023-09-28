dfe94c59f89f57bb8a5cfd5f3d08cbbebb8d4bfc
Denis <denisk@bugsec.com>
2023-09-28 16:51:55 +0300

    2.2.6

--- atomicshop/dns.py
+++ atomicshop/dns.py
@@ -1,3 +1,8 @@
+import dns.resolver
+
+from .print_api import print_api
+
+
 # Defining Dictionary of Numeric to String DNS Query Types.
 # https://en.wikipedia.org/wiki/List_of_DNS_record_types
 TYPES_DICT = {
@@ -21,3 +26,42 @@ ETW_DNS_INFO = {
     # is received and not After DNS Query is sent.
     'event_id': 3008
 }
+
+
+def resolve_dns_localhost(domain_name: str, dns_servers_list: list = None, print_kwargs: dict = None) -> str:
+    """
+    Resolve DNS queries when the DNS gateway is localhost.
+
+    :param domain_name: string of Domain name to resolve.
+    :param dns_servers_list: List of DNS services (IPv4 strings) to resolve.
+    :param print_kwargs: dict, that contains all the arguments for 'print_api' function.
+
+    :return: the first result of the DNS query.
+    """
+
+    # If 'dns_servers_list' is empty, assign Google DNS server by default.
+    if not dns_servers_list:
+        dns_servers_list = ['8.8.8.8']
+
+    # noinspection PyTypeChecker
+    connection_ip: str = None
+
+    try:
+        # The class should be called separately for each thread. You can't create it in the main thread and
+        # pass it to threads as object.
+        # Building DNS Resolver, it will receive DNS servers from configuration file to contact
+        resolver = dns.resolver.Resolver()
+        # Assigning the dns service address we acquired from configuration file to resolver
+        resolver.nameservers = dns_servers_list
+        # Get the DNS
+        function_server_address = resolver.resolve(domain_name, 'A')
+        # Get only the first entry of the list of IPs [0]
+        connection_ip = function_server_address[0].to_text()
+        message = f"Resolved to [{connection_ip}]"
+        print_api(message, **print_kwargs)
+    except dns.resolver.NXDOMAIN:
+        message = f"Domain {domain_name} doesn't exist - Couldn't resolve with {dns_servers_list}."
+        print_api(message, **print_kwargs, error_type=True, logger_method='error')
+        pass
+
+    return connection_ip
