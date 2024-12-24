49d45475d02b294fa6cd5c620d565f183cf86b64
Denis Kras <denisk@bugsec.com>
2024-12-24 16:15:22 +0200

    2.18.20

--- atomicshop/wrappers/psutilw/processes.py
+++ atomicshop/wrappers/psutilw/processes.py
@@ -1,7 +1,10 @@
-import psutil
+import os
 import time
 
+import psutil
+
 from ...print_api import print_api
+from ..ctyping import file_details_winapi
 
 
 def wait_for_process(pid: int):
@@ -43,3 +46,36 @@ def kill_process_by_pid(pid: int, print_kwargs: dict = None):
     except psutil.TimeoutExpired:
         # print(f"Process {pid} did not terminate in time.")
         pass
+
+
+def get_running_processes_with_exe_info() -> list[dict]:
+    """
+    Retrieve information about all running processes on the system.
+    """
+    processes_info: list[dict] = []
+
+    for proc in psutil.process_iter(attrs=["pid", "name", "exe"]):
+        try:
+            pid = proc.info["pid"]
+            name = proc.info["name"]
+            exe = proc.info["exe"]
+
+            if exe and os.path.isfile(exe):
+                # Get file properties
+                file_properties = file_details_winapi.get_file_properties(exe)
+
+                # Add process info to the list
+                processes_info.append({
+                    "PID": pid,
+                    "Name": name,
+                    "FilePath": exe,
+                    "FileDescription": file_properties["FileDescription"],
+                    "FileVersion": file_properties["FileVersion"],
+                    "ProductName": file_properties["ProductName"],
+                    "ProductVersion": file_properties["ProductVersion"],
+                })
+        except (psutil.AccessDenied, psutil.NoSuchProcess, psutil.ZombieProcess):
+            # Skip processes that cannot be accessed
+            continue
+
+    return processes_info
